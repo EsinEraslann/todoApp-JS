2a233d4ee17941d8fb9289be5b8916e0f9663cdf
EsinEraslann <esineraslanx@gmail.com>
2024-02-28 19:44:13 +0300

    button add

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 "use strict"
 
-let sonuc;
-
 let gorevListesi = [
     { "id": 1, "gorevAdi": "Görev 1" },
     { "id": 2, "gorevAdi": "Görev 2" },
@@ -9,19 +7,54 @@ let gorevListesi = [
     { "id": 4, "gorevAdi": "Görev 4" },
 ];
 
-let ul = document.getElementById("task-list");
+// form kısmını kodluyoruz
 
-for (let gorev of gorevListesi) {
+displayTasks();
 
-    let li = `
-        <li class="task list-group-item">
-            <div class="form-check">
-                <input type="checkbox" id="${gorev.id}" class="form-check-input">
-                    <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
-            </div>
-        </li >
-`;
+function displayTasks() {
+    let ul = document.getElementById("task-list");
+    ul.innerHTML = "";
 
-    ul.insertAdjacentHTML("beforeend", li);
+    for (let gorev of gorevListesi) {
+
+        let li = `
+                <li class="task list-group-item">
+                    <div class="form-check">
+                        <input type="checkbox" id="${gorev.id}" class="form-check-input">
+                            <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+                    </div>
+                </li >
+        `;
+
+        // içerikleri en sona ekleyebileceğimiz kalıp 
+
+        ul.insertAdjacentHTML("beforeend", li);
+    }
+}
+
+
+document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+document.querySelector("#btnAddNewTask").addEventListener("keypress", function (){
+/*     event.preventDefault(); */
+    if (event.key == "Enter") {
+        document.getElementById("btnAddNewTask").click();
+    }
+});
+
+
+
+function newTask(event) {
+
+    let taskInput = document.querySelector("#txtTaskName");
+
+    if (taskInput.value == "") {
+        alert("görev girmelisiniz")
+    } else {
+        gorevListesi.push({ "id": gorevListesi.length + 1, "gorevAdi": taskInput.value });
+        taskInput.value = "";
+        displayTasks();
+    }
+
+    event.preventDefault();
 }
 
